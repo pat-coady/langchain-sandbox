0502ba5d5212286b674dddd492801f3e503319be
Patrick Coady <pcoady00@gmail.com>
2024-05-29 12:32:22 +0000

    build out setup.sh and README

--- setup.sh
+++ setup.sh
@@ -1,11 +1,29 @@
-# remove this plugin that GCP includes in jupyter install
-# causes a bunch of warnings
-pip uninstall dataproc_jupyter_plugin
+#!/bin/bash
 
-# point to jupyter config in working directory
-# sets ip = '*', port='8888', open_browser=False
-export JUPYTER_CONFIG_DIR="."
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y python3-venv
+echo "source langchain-sandbox/venv/bin/activate" >> ~/.bashrc
 
-# langchain ENV variables
-export LANGCHAIN_TRACING_V2="true"
+ENV_VARS=(
+    "export JUPYTER_CONFIG_DIR='.'"
+    "export LANGCHAIN_TRACING_V2='true'"
+)
+
+add_env_vars_to_bashrc() {
+    for var in "${ENV_VARS[@]}"; do
+        # Check if the variable is already in .bashrc
+        if ! grep -q "$var" ~/.bashrc; then
+            echo "$var" >> ~/.bashrc
+            echo "Added: $var"
+        else
+            echo "Already present: $var"
+        fi
+    done
+}
+
+add_env_vars_to_bashrc
+source ~/.bashrc
+
+pip install -r requirements.txt
 
